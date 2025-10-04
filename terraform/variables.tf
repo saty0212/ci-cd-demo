3109e7f4ac1fc863351ebad188bfb36c3817efa8
saty0212 <63355364+saty0212@users.noreply.github.com>
2025-10-04 07:24:10 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,15 +7,6 @@ variable "aws_region" {
 variable "key_name" {
   description = "EC2 key pair name (optional for SSH)"
   type        = string
-  default     = null
+  default     = ""
 }
 
-variable "aws_access_key" {
-  description = "AWS access key for authentication"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key for authentication"
-  type        = string
-}
